18ec7e46323c985fc373bc18a4044a93403e529e
Ian Pendleton <ipendlet@umich.edu>
2018-11-14 13:58:44 -0500

    update for debugging

--- runme.py
+++ runme.py
@@ -1,28 +1,28 @@
 #Copyright (c) 2018 Ian Pendleton - MIT License
-from expworkup import jsontocsv
-from expworkup import createjson
-import argparse as ap
-import os
-from expworkup import googleio
-
-# Some command line interfacing to aid in script handling
-parser = ap.ArgumentParser(description='Target Folder')
-parser.add_argument('folder', type=str, help='Please include target folder') 
-
-## Debug option for future use?
-#parser.add_argument('--Debug', type=int, help='Debug=1 prints raw to csv, debug=0 prints only data for learning (default=0)', default=0)
-
-args = parser.parse_args()
-myjsonfol = args.folder
-
-#debug = args.Debug
-
-#Ensure directories are in order
-if not os.path.exists('data/datafiles'):
-    os.mkdir('data/datafiles')
-if not os.path.exists(myjsonfol):
-    os.mkdir(myjsonfol)
-
-#run the main body of the code.  Can be called later as a module if needed
-createjson.ExpDirOps(myjsonfol) #Run Primary JSON Creator
-jsontocsv.printfinal(myjsonfol) # RUn the JSON to CSV parser
+if __name__ == "__main__":
+    from expworkup import jsontocsv
+    from expworkup import createjson
+    import argparse as ap
+    import os
+    from expworkup import googleio
+    
+    # Some command line interfacing to aid in script handling
+    parser = ap.ArgumentParser(description='Target Folder')
+    parser.add_argument('folder', type=str, help='Please include target folder') 
+    parser.add_argument('-d', '--debug', type=int, default=0, help='Turns on testing for implementing new features to the front end of the code, prior to distribution through dataset')
+    
+    args = parser.parse_args()
+    myjsonfol = args.folder
+    debug = args.debug
+    
+    #debug = args.Debug
+    
+    #Ensure directories are in order
+    if not os.path.exists('data/datafiles'):
+        os.mkdir('data/datafiles')
+    if not os.path.exists(myjsonfol):
+        os.mkdir(myjsonfol)
+    
+    #run the main body of the code.  Can be called later as a module if needed
+    createjson.ExpDirOps(myjsonfol, debug) #Run Primary JSON Creator
+    jsontocsv.printfinal(myjsonfol, debug) # RUn the JSON to CSV parser
