e952bd43a7e382a0b03a3b9d300c50bd4aef87f9
Ian Pendleton <ipendleton@haverford.edu>
2020-05-14 12:29:13 -0400

    readability, streamline feats

--- expworkup/handlers/feature_generator.py
+++ expworkup/handlers/feature_generator.py
@@ -201,6 +201,10 @@ class OneTypeFeatures():
                                                    self.smiles_list,
                                                    self.one_type,
                                                    False)
+<<<<<<< HEAD
+=======
+            outdf.rename(columns={"Compound": "calc_input_smiles"}, inplace=True)
+>>>>>>> readability, streamline feats
             outdf = pd.concat([outdf,
                                type_features_df], axis=1)
 
@@ -209,7 +213,11 @@ class OneTypeFeatures():
                                                    self.smiles_list,
                                                    self.one_type,
                                                    True)
+<<<<<<< HEAD
             type_features_df.rename(columns={"Compound": "smiles_standardized"}, inplace=True)
+=======
+            outdf.rename(columns={"Compound": "smiles_standardized"}, inplace=True)
+>>>>>>> readability, streamline feats
             outdf = pd.concat([outdf,
                                type_features_df], axis=1)
 
