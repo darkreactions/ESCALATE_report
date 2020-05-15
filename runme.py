631f02662586dbfbfde536629d45239af7b20616
Ian Pendleton <ipendleton@haverford.edu>
2020-05-15 11:07:34 -0400

    resolved error with combine_first function, _raw_smiles export enabled

--- runme.py
+++ runme.py
@@ -219,9 +219,9 @@ def main_pipeline(args):
         sys.exit()
 
     # Perform feature calculations / gather physicochemical descriptors
-#    compound_ingredient_objects_df = ingredient_pipeline(report_df,
-#                                                         chemdf_dict,
-#                                                         args.debug)
+    compound_ingredient_objects_df = ingredient_pipeline(report_df,
+                                                         chemdf_dict,
+                                                         args.debug)
 
     runUID_inchi_file,\
         inchi_key_indexed_features_df= feat_pipeline(get_target_folder(),
@@ -230,8 +230,6 @@ def main_pipeline(args):
                                                      args.debug,
                                                      get_log_folder())
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     calc_out_df = calc_pipeline(report_df,
                                 compound_ingredient_objects_df,
                                 chemdf_dict,
@@ -248,28 +246,6 @@ def main_pipeline(args):
                                           args.raw)
 
     escalate_final_df.to_csv(f'{get_target_folder()}.csv')
-=======
-=======
->>>>>>> readability, streamline feats
-#    calc_out_df = calc_pipeline(report_df,
-#                                compound_ingredient_objects_df,
-#                                chemdf_dict,
-#                                args.debug) 
-#    #calc_out_df.to_csv(f'./{args.local_directory}/offline/REPORT_CALCOUT.csv')
-#    #calc_out_df = pd.read_csv(f'./{args.local_directory}/offline/REPORT_CALCOUT.csv')
-#
-#    # Export dataframe
-#    escalate_final_df = construct_2d_view(report_df,
-#                                          calc_out_df,
-#                                          inchi_key_indexed_features_df, 
-#                                          args.debug,
-#                                          args.raw)
-#
-#    escalate_final_df.to_csv(f'{get_target_folder()}.csv')
-<<<<<<< HEAD
->>>>>>> readability, streamline feats
-=======
->>>>>>> readability, streamline feats
 
     # Additional variations on dataframe export (for escalation / versioned data repo)
     if ('state' in vars(args)):
