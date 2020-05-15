c552e3c3ebf9c673ab70ff3be11bb83200448828
Ian Pendleton <ipendleton@haverford.edu>
2020-05-15 12:40:42 -0400

    conflict resolution

--- expworkup/handlers/feature_generator.py
+++ expworkup/handlers/feature_generator.py
@@ -201,10 +201,6 @@ class OneTypeFeatures():
                                                    self.smiles_list,
                                                    self.one_type,
                                                    False)
-<<<<<<< HEAD
-=======
-            outdf.rename(columns={"Compound": "calc_input_smiles"}, inplace=True)
->>>>>>> d592d6d8d501694e1e0249bd2eb33e3a58f81884
             outdf = pd.concat([outdf,
                                type_features_df], axis=1)
 
@@ -213,11 +209,7 @@ class OneTypeFeatures():
                                                    self.smiles_list,
                                                    self.one_type,
                                                    True)
-<<<<<<< HEAD
             type_features_df.rename(columns={"Compound": "smiles_standardized"}, inplace=True)
-=======
-            outdf.rename(columns={"Compound": "smiles_standardized"}, inplace=True)
->>>>>>> d592d6d8d501694e1e0249bd2eb33e3a58f81884
             outdf = pd.concat([outdf,
                                type_features_df], axis=1)
 
